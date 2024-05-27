6d65b39a9209a1eeb12461f4c661b3f814b24127
Saymen930 <115408305+saymen930@users.noreply.github.com>
2024-05-27 21:52:26 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "6593983099:AAFHOVsCGMN_qI7Vhov5iLuFVVRBiSYe7Lk"
+TOKEN = "6761753644:AAGfrsu3AEL4NN26UW1SIDACwWAsrgV93vE"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
